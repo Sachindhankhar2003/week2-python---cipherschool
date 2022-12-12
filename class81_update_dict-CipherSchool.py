467907f0a8b156906b0a77da305cb23a50db017d
SACHIN DHANKHAR <114800115+Sachindhankhar2003@users.noreply.github.com>
2022-12-12 23:16:50 +0530

    Add files via upload

---
+++ class81_update_dict-CipherSchool.py
@@ -0,0 +1,10 @@
+user_info = {
+    'name' : 'Sachin dhankhar' ,
+    'age' : 19 ,
+    "fav movies" : "moonknight"
+}
+
+more_info = {"state" : "Rajasthan" , "hobbies":["coding","reading","cricket"]}
+
+user_info.update(more_info)
+print(user_info)
